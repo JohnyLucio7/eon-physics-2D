ecfdafe1e9b2079a206c2284dad3bdd854ea88f7
Johny Lúcio <59095834+JohnyLucio7@users.noreply.github.com>
2026-06-04 17:13:41 -0300

    perf: use inverse mass for acceleration calculation

--- src/Physics/Particle.cpp
+++ src/Physics/Particle.cpp
@@ -5,6 +5,14 @@ Particle::Particle(float x, float y, float mass)
 {
     this->position = Vec2(x, y);
     this->mass = mass;
+    if (mass != 0.0)
+    {
+        this->invMass = 1.0 / mass;
+    }
+    else
+    {
+        this->invMass = 0.0;
+    }
     std::cout << "Particle constructor called!" << std::endl;
 }
 
@@ -13,17 +21,20 @@ Particle::~Particle()
     std::cout << "Particle destructor called!" << std::endl;
 }
 
-void Particle::AddForce(const Vec2& force) {
+void Particle::AddForce(const Vec2& force)
+{
     sumForces += force;
 }
 
-void Particle::ClearForces() {
+void Particle::ClearForces()
+{
     sumForces = Vec2(0.0, 0.0);
 }
 
-void Particle::Integrate(float dt) {
+void Particle::Integrate(float dt)
+{
     // Find the acceleration based on the forces that are being applied and the mass
-    acceleration = sumForces / mass;
+    acceleration = sumForces * invMass;
 
     // Integrate the acceleration to find the new velocity
     velocity += acceleration * dt;
