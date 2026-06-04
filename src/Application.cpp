20c56dcadbefe64287ac70ac874ffe6716eb607b
Johny Lúcio <59095834+JohnyLucio7@users.noreply.github.com>
2026-06-04 19:06:24 -0300

    feat: add particle spawning on mouse click and adjust drag coefficient

--- src/Application.cpp
+++ src/Application.cpp
@@ -14,14 +14,6 @@ void Application::Setup()
 {
     running = Graphics::OpenWindow();
 
-    Particle* smallBall = new Particle(50, 100, 1.0);
-    smallBall->radius = 4;
-    particles.push_back(smallBall);
-
-    Particle* bigBall = new Particle(200, 100, 3.0);
-    bigBall->radius = 12;
-    particles.push_back(bigBall);
-
     liquid.x = 0;
     liquid.y = Graphics::Height() / 2;
     liquid.w = Graphics::Width();
@@ -63,6 +55,16 @@ void Application::Input()
             if (event.key.keysym.sym == SDLK_LEFT)
                 pushForce.x = 0;
             break;
+        case SDL_MOUSEBUTTONDOWN:
+            if (event.button.button == SDL_BUTTON_LEFT)
+            {
+                int x, y;
+                SDL_GetMouseState(&x, &y);
+                Particle* particle = new Particle(x, y, 1.0);
+                particle->radius = 5;
+                particles.push_back(particle);
+            }
+            break;
         }
     }
 }
@@ -70,7 +72,8 @@ void Application::Input()
 ///////////////////////////////////////////////////////////////////////////////
 // Update function (called several times per second to update objects)
 ///////////////////////////////////////////////////////////////////////////////
-void Application::Update() {
+void Application::Update()
+{
     // Wait some time until the reach the target frame time in milliseconds
     static int timePreviousFrame;
     int timeToWait = MILLISECS_PER_FRAME - (SDL_GetTicks() - timePreviousFrame);
@@ -86,41 +89,48 @@ void Application::Update() {
     timePreviousFrame = SDL_GetTicks();
 
     // Apply forces to the particles
-    for (auto particle: particles) {
-        Vec2 wind = Vec2(0.2 * PIXELS_PER_METER, 0.0);
-        particle->AddForce(wind);
-
+    for (auto particle : particles)
+    {
         Vec2 weight = Vec2(0.0, particle->mass * 9.8 * PIXELS_PER_METER);
         particle->AddForce(weight);
 
         particle->AddForce(pushForce);
 
         // Apply a drag force if we are inside the liquid...
-        if (particle->position.y >= liquid.y) {
-            Vec2 drag = Force::GenerateDragForce(*particle, 0.04);
+        if (particle->position.y >= liquid.y)
+        {
+            Vec2 drag = Force::GenerateDragForce(*particle, 0.03);
             particle->AddForce(drag);
         }
     }
 
     // Integrate the acceleration and velocity to estimate the new position
-    for (auto particle: particles) {
+    for (auto particle : particles)
+    {
         particle->Integrate(deltaTime);
     }
 
     // Check the boundaries of the window
-    for (auto particle: particles) {
+    for (auto particle : particles)
+    {
         // Nasty hardcoded flip in velocity if it touches the limits of the screen window
-        if (particle->position.x - particle->radius <= 0) {
+        if (particle->position.x - particle->radius <= 0)
+        {
             particle->position.x = particle->radius;
             particle->velocity.x *= -0.9;
-        } else if (particle->position.x + particle->radius >= Graphics::Width()) {
+        }
+        else if (particle->position.x + particle->radius >= Graphics::Width())
+        {
             particle->position.x = Graphics::Width() - particle->radius;
             particle->velocity.x *= -0.9;
         }
-        if (particle->position.y - particle->radius <= 0) {
+        if (particle->position.y - particle->radius <= 0)
+        {
             particle->position.y = particle->radius;
             particle->velocity.y *= -0.9;
-        } else if (particle->position.y + particle->radius >= Graphics::Height()) {
+        }
+        else if (particle->position.y + particle->radius >= Graphics::Height())
+        {
             particle->position.y = Graphics::Height() - particle->radius;
             particle->velocity.y *= -0.9;
         }
@@ -141,6 +151,7 @@ void Application::Render()
     {
         Graphics::DrawFillCircle(particle->position.x, particle->position.y, particle->radius, 0xFFFFFFFF);
     }
+
     Graphics::RenderFrame();
 }
 
