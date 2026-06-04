43040ab4abd351f87cd2d5fe34113881fcceda4f
Johny Lúcio <59095834+JohnyLucio7@users.noreply.github.com>
2026-06-04 19:21:03 -0300

    feat: implement slingshot launch mechanic and friction force

--- src/Application.h
+++ src/Application.h
@@ -5,14 +5,13 @@
 #include "./Physics/Particle.h"
 #include <vector>
 
-class Application
-{
+class Application {
 private:
     bool running = false;
-
     std::vector<Particle*> particles;
     Vec2 pushForce = Vec2(0, 0);
-    SDL_Rect liquid;
+    Vec2 mouseCursor = Vec2(0, 0);
+    bool leftMouseButtonDown = false;
 
 public:
     Application() = default;
